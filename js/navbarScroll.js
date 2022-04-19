ee0e3e759b09ba12211cc43b3adbaf5a4ac15a4f
João Victor Garcia <joao.garcia18@fatec.sp.gov.br>
2022-04-19 17:24:35 -0300

    adding new features

---
+++ js/navbarScroll.js
@@ -0,0 +1,10 @@
+window.addEventListener('scroll', function () {
+    var posicaoy = window.pageYOffset;
+    const navScroll = document.getElementById('navbarScroll')
+  
+    if (posicaoy >= 200) {
+        navScroll.classList.add('nav-scroll')
+    } else if (posicaoy < 200) {
+        navScroll.classList.remove('nav-scroll')
+    }
+});
