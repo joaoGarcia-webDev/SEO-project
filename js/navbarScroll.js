48470aae1fe9fe70280ab980e6fecc7cbd2686e6
João Victor Garcia <joao.garcia18@fatec.sp.gov.br>
2022-04-20 17:16:07 -0300

    final updates

--- js/navbarScroll.js
+++ js/navbarScroll.js
@@ -1,7 +1,7 @@
 window.addEventListener('scroll', function () {
     var posicaoy = window.pageYOffset;
     const navScroll = document.getElementById('navbarScroll')
-  
+    
     if (posicaoy >= 200) {
         navScroll.classList.add('nav-scroll')
     } else if (posicaoy < 200) {
